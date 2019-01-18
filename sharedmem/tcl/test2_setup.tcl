4b499d471874414b08a193d20ff515b8cbcca5ac
Matthew Lai <mmlai@sympatico.ca>
2019-01-17 21:32:23 -0500

    minor code changes

--- sharedmem/tcl/test2_setup.tcl
+++ sharedmem/tcl/test2_setup.tcl
@@ -48,6 +48,8 @@ source msgdef.tcl
 Msgdef::Init
 set msgfile $env(COMP_HOME)/msgdef/test/test0.msg
 set msgname [Msgdef::Parse $msgfile]
+array set msgattr {}
+Msgdef::Get_Attr_Offset $msgname msgattr
 
 load $env(TCLSHAREDMEM)/tclsharedmem.so tclsharedmem
 
@@ -62,78 +64,102 @@ port_mgr_add out2 $size
 stub_init test_tx.tcl $len $size 
 stub_clear test_tx.tcl $len $size
 
-port_mgr_msg_set out1 aaaaaaaa 0 
-port_mgr_msg_set out1 bbbbbbbb 10 
-port_mgr_msg_set out1 cccccccc 20 
-port_mgr_msg_set out1 dddddddd 30
-port_mgr_msg_set out1 test0 40
-port_mgr_msg_set out1 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out1 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_tx.tcl [port_mgr_get_msg out1]
 
-port_mgr_msg_set out1 11111111 0 
-port_mgr_msg_set out1 22222222 10 
-port_mgr_msg_set out1 33333333 20 
-port_mgr_msg_set out1 44444444 30
-port_mgr_msg_set out1 test0 40
-port_mgr_msg_set out1 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+set msgdata(f1) 11111111
+set msgdata(f2) 22222222 
+set msgdata(f3) 33333333 
+set msgdata(f4) 44444444 
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out1 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_tx.tcl [port_mgr_get_msg out1]
 
-port_mgr_msg_set out1 eeeeeeee 0 
-port_mgr_msg_set out1 ffffffff 10 
-port_mgr_msg_set out1 gggggggg 20 
-port_mgr_msg_set out1 hhhhhhhh 30
-port_mgr_msg_set out1 test0 40
-port_mgr_msg_set out1 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+set msgdata(f1) eeeeeeee 
+set msgdata(f2) ffffffff 
+set msgdata(f3) gggggggg 
+set msgdata(f4) hhhhhhhh 
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out1 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_tx.tcl [port_mgr_get_msg out1]
 
-port_mgr_msg_set out1 55555555 0 
-port_mgr_msg_set out1 66666666 10 
-port_mgr_msg_set out1 77777777 20 
-port_mgr_msg_set out1 88888888 30
-port_mgr_msg_set out1 test0 40
-port_mgr_msg_set out1 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+set msgdata(f1) 55555555 
+set msgdata(f2) 66666666 
+set msgdata(f3) 77777777 
+set msgdata(f4) 88888888 
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out1 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_tx.tcl [port_mgr_get_msg out1]
 
 stub_init test_rx.tcl $len $size 
 stub_clear test_rx.tcl $len $size
 
-port_mgr_msg_set out2 aaaaaaaa 0 
-port_mgr_msg_set out2 bbbbbbbb 10 
-port_mgr_msg_set out2 cccccccc 20 
-port_mgr_msg_set out2 dddddddd 30
-port_mgr_msg_set out2 test0 40
-port_mgr_msg_set out2 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out2 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_rx.tcl [port_mgr_get_msg out2]
 
-port_mgr_msg_set out2 11111111 0 
-port_mgr_msg_set out2 22222222 10 
-port_mgr_msg_set out2 33333333 20 
-port_mgr_msg_set out2 44444444 30
-port_mgr_msg_set out2 test0 40
-port_mgr_msg_set out2 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+set msgdata(f1) 11111111 
+set msgdata(f2) 22222222 
+set msgdata(f3) 33333333 
+set msgdata(f4) 44444444 
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out2 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_rx.tcl [port_mgr_get_msg out2]
 
-port_mgr_msg_set out2 eeeeeeee 0 
-port_mgr_msg_set out2 ffffffff 10 
-port_mgr_msg_set out2 gggggggg 20 
-port_mgr_msg_set out2 hhhhhhhh 30
-port_mgr_msg_set out2 test0 40
-port_mgr_msg_set out2 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+set msgdata(f1) eeeeeeee 
+set msgdata(f2) ffffffff 
+set msgdata(f3) gggggggg 
+set msgdata(f4) hhhhhhhh 
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out2 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_rx.tcl [port_mgr_get_msg out2]
 
-port_mgr_msg_set out2 55555555 0 
-port_mgr_msg_set out2 66666666 10 
-port_mgr_msg_set out2 77777777 20 
-port_mgr_msg_set out2 88888888 30
-port_mgr_msg_set out2 test0 40
-port_mgr_msg_set out2 0.1 50
+array set msgdata {}
+Msgdef::Factory $msgname msgdata
+set msgdata(f1) 55555555 
+set msgdata(f2) 66666666 
+set msgdata(f3) 77777777 
+set msgdata(f4) 88888888 
+foreach idx [array names msgdata] {
+    port_mgr_msg_set out2 $msgdata($idx) $msgattr($idx)
+}
+unset msgdata
 
 sv_csr_write_wrapper test_rx.tcl [port_mgr_get_msg out2]
 
