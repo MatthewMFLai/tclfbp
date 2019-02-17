031bc7fa9fe4111242cddd08e558d2dc2bb0c03c
Matthew Lai <mmlai@sympatico.ca>
2019-02-17 09:28:17 -0500

    code changes for sock node feature

--- sharedmem/tcl/sock_node.tcl
+++ sharedmem/tcl/sock_node.tcl
@@ -107,3 +107,202 @@ proc Admin_Client_Handle {cid} {
 #   puts $s "Hello!"
 #   gets $s line
 
+#--------------------------------------------------
+# Socket interface code section
+
+proc client_init {cid} {
+    fileevent $cid readable "client_handle $cid"
+    fconfigure $cid -buffering line
+}
+
+proc client_handle {cid} {
+    if {[gets $cid request] < 0} {
+	global main-loop
+        close $cid
+	set main-loop 1
+    } else {
+        # Custom code to handle initialization.
+        #
+	set response ""
+        if {[initialize $request response]} {
+	    puts $cid $response
+	    flush $cid
+	}
+    }
+}
+
+proc initialize {request p_response} {
+    global g_running
+
+    upvar $p_response response
+    set response ""
+    set rc 0 
+
+    switch -- [lindex $request 0] {
+        INIT {
+            set response [app_init] 
+            set rc 1 
+        }
+        ENABLE {
+            set g_running 1
+            set response "App ruuning"
+            set rc 1 
+        }
+        DISABLE {
+            set g_running 0 
+            set response "App stopped"
+            set rc 1 
+        }
+        TEST {
+            set response [app_test] 
+            set rc 1 
+        }
+	default {
+            set response "$request not recognized"
+            set rc 1 
+        }
+    }
+    return $rc
+}
+# End Socket interface code section
+#-------------------------------------------------------------------
+proc process {} {
+
+    set bcd_msg [mem_read $key] 
+    sock_write $key $bcd_msg 
+    return
+}
+
+proc app_test {} {
+    global g_msgcount
+
+    return $g_msgcount
+}
+
+proc app_init {} {
+    global g_msgcount
+
+    set g_msgcount 0
+    return "OK"
+}
+#-------------------------------------------------------------------
+proc mem_read {key} {
+
+    set rc 1 
+    while {$rc != 0} {
+        set rc [sv_csr_read_wrapper $key [key_mgr_get_msg $key]]
+        if {$rc == 0} {
+            break
+        }
+        yield
+    }
+    return [key_mgr_bcd_get $key]
+}
+
+proc mem_write {key p_msgout} {
+    upvar $p_msgout msgout
+
+    Portmgr::Get_Portmsgdef $port msgname
+    array set tmpdata {}
+    Msgdef::Get_Attr_Offset $msgname tmpdata
+    foreach attr [array names tmpdata] {
+        if {![info exists msgout($attr)]} {
+            continue
+        }
+        port_mgr_msg_set $port $msgout($attr) $tmpdata($attr)
+    }
+
+    Portmgr::Get_Shm $port keylist
+    set rc 1 
+    while {$rc != 0} {
+        foreach shmkey $keylist {
+            set rc [sv_csr_write_wrapper $shmkey [port_mgr_get_msg $port]]
+            if {$rc == 0} {
+                break
+            }
+        }
+        if {$rc} {
+            yield
+        }
+    }
+}
+
+proc sock_write {key bcd_msg} {
+    global g_coroutines
+
+    set idx [array names g_coroutines $key-*]
+    set sd [lindex [split $idx "-"] 1]
+    puts $sd $bcd_msg
+    set response [yield]
+    if {$response != "OK $bcd_msg"} {
+        puts "Invalid response: $response"
+    }
+    return 
+}
+
+proc runit {} {
+    yield
+
+    set count 10
+    while {1} {
+
+        incr count -1
+        process $count
+        if {$count == 0} {
+            set count 10
+        }
+    }
+ 
+    return
+}
+
+proc checkagain {} {
+    global g_running
+
+    if {$g_running} {
+        checkit
+    }
+    after 10 checkagain
+}
+
+global g_coroutines
+array set g_coroutines {}
+
+load $env(TCLSHAREDMEM)/tclsharedmem.so tclsharedmem 
+queue_init
+key_mgr_init
+
+# argument data looks like this:
+# tclsh node_socif.tcl BLOCK s0:source0 INIT localhost:8000 KEYS-OUT {<key>:<size>:<len>:<ipaddr>:<port> <key>:<size>:<len>:<ipaddr>:<port> ... } KEYS-IN {<key>:<size>:<len> ... } 
+#
+array set argdata $argv 
+
+# All keys are suffixed by "IN-" and "OUT-"
+foreach keydata $argdata(KEYS-OUT) { 
+    set tokens [split $keydata ":"]
+    set key [lindex $tokens 0]
+    set size [lindex $tokens 1]
+    set len [lindex $tokens 2]
+    set ipaddr [lindex $tokens 3]
+    set port [lindex $tokens 4]
+
+    key_mgr_add $key $size
+    stub_init $key $len $size
+    set sd [Echo_Client $ipaddr $port]
+    coroutine $key-$sd runit
+    set g_coroutines($key-$sd) $key-$sd 
+} 
+
+set g_running $argdata(RUNNING)
+
+set port [lindex [split $argdata(INIT) ":"] 1]
+set sd [socket localhost $port]
+client_init $sd
+
+coroutine checkit runit
+
+after idle checkagain
+
+vwait main-loop 
+
+exit
