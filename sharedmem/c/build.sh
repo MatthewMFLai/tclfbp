74a8c59bac831174ab36fdf1bb1b5967f7f4f7dd
lai <mmlai@sympatico.ca>
2019-03-16 20:53:59 -0400

    use /home/ubuntu to locate tclfbp and extend include path in build.sh

--- sharedmem/c/build.sh
+++ sharedmem/c/build.sh
@@ -1,3 +1,3 @@
 swig -tcl test.i
-gcc -c -fpic test_wrap.c queue.c shm.c shm_mgr.c ringbuff.c test_helper.c sock_helper.c -I. -I/usr/local/include -I/opt/ActiveTcl-8.6/include -save-temps
+gcc -c -fpic test_wrap.c queue.c shm.c shm_mgr.c ringbuff.c test_helper.c sock_helper.c -I. -I/usr/local/include -I/opt/ActiveTcl-8.6/include -I/usr/include/tcl8.6 -save-temps
 gcc -shared test_wrap.o queue.o shm.o shm_mgr.o ringbuff.o test_helper.o sock_helper.o -lrt -o tclsharedmem.so
