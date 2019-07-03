c3c8a6b7bd153da2c881db5ea01f33fd8ec0b710
lai <mmlai@sympatico.ca>
2019-07-03 19:19:19 -0400

    add test2 for company to test fetching company info for each TO symbol from the symbol data file

---
+++ component/company/test2/test.tcl
@@ -0,0 +1,39 @@
+proc process {count} {
+    global g_symbols
+
+	foreach symbol $g_symbols {
+
+    	array set msgout {}
+    	port_factory_msg OUT-1 msgout
+    	set msgout(exchange) "TO"
+    	set msgout(symbol) $symbol 
+    	port_write OUT-1 msgout
+
+    	array set msgin {}
+    	port_read IN-1 msgin
+		puts "$msgin(symbol) $msgin(description)"
+    	unset msgout
+		unset msgin
+    }
+	client_send "TESTEND"
+
+    return
+}
+
+proc app_test {} {
+    global g_msgcount
+
+    return $g_msgcount
+}
+
+proc app_init {} {
+    global env g_symbols
+
+    set fd [open $env(COMP_HOME)/company/test2/symbols_to.dat r]
+    set g_symbols ""
+    while {[gets $fd line] > -1} {
+		lappend g_symbols [lindex $line 0]
+	}
+	close $fd
+    return "OK"
+}
