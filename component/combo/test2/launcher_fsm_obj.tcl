ae246bd1bd0fb5dde9d24ebdc639f0289c49ca9a
lai <mmlai@sympatico.ca>
2019-08-08 17:06:42 -0400

    fsm object code changes

--- component/combo/test2/launcher_fsm_obj.tcl
+++ component/combo/test2/launcher_fsm_obj.tcl
@@ -1,24 +1,70 @@
 namespace eval OBJNAME_FSM {
 
+variable m_cid
+variable m_cmd
+variable m_data
+
 proc init {} {
+	variable m_cid
+	variable m_cmd
+	variable m_data
+
+    set m_cid ""
+    set m_cmd ""
+    set m_data ""
+	return
 }
 
 proc process_generic {p_arg_array} {
+	variable m_cid
+	variable m_cmd
+	variable m_data
     upvar $p_arg_array arg_array
 
+	set m_cid $arg_array(cid)
+	set m_cmd $arg_array(cmd)
+	set m_data $arg_array(data) 
     return
 }
 
+proc eval_ident_to_init {} {
+	variable m_cmd
+
+	if {$m_cmd == "IDENT"} {
+		return 1
+	}
+    return 0
+}
+
 proc eval_init_to_enable {} {
+	variable m_cmd
+
+	if {$m_cmd == "INIT"} {
+		return 1
+	}
     return 0
 }
 
 proc eval_enable_to_testend {} {
+	variable m_cmd
+
+	if {$m_cmd == "ENABLE"} {
+		return 1
+	}
     return 0
 }
 
 proc eval_testend_to_shutdown {} {
+	variable m_cmd
+
+	if {$m_cmd == "TESTEND"} {
+		return 1
+	}
     return 0
+}
+
+proc act_iden_to_init {} {
+    return
 }
 
 proc act_init_to_enable {} {
