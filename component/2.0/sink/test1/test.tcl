322f51e5c6c67729ba416179ba53336647b8604f
matthewlai <mmlai@sympatico.ca>
2019-10-17 11:32:19 -0400

    trim test code for sink component

--- component/2.0/sink/test1/test.tcl
+++ component/2.0/sink/test1/test.tcl
@@ -1,71 +1,7 @@
 proc process {} {
 
-    array set msgin {}
     port_read IN-1 msgin
-    if {$msgin(f1) != "aaaa"} {
-		client_send "TESTFAIL"
-		return
-    }
-    port_write OUT-1 msgin
-	unset msgin
-
-    array set msgin {}
-    port_read IN-1 msgin
-    if {$msgin(f1) != "bbbb"} {
-		client_send "TESTFAIL"
-		return
-    }
-    port_write OUT-1 msgin
-	unset msgin
-
-    array set msgin {}
-    port_read IN-1 msgin
-    if {$msgin(f1) != "cccc"} {
-		client_send "TESTFAIL"
-		return
-    }
-    port_write OUT-1 msgin
-	unset msgin
-
-    array set msgin {}
-    port_read IN-1 msgin
-    if {$msgin(f1) != "dddd"} {
-		client_send "TESTFAIL"
-		return
-    }
-    port_write OUT-1 msgin
-	unset msgin
-
-    array set msgin {}
-    port_read IN-1 msgin
-    if {$msgin(f1) != "eeee"} {
-		client_send "TESTFAIL"
-		return
-    }
-    port_write OUT-1 msgin
-	unset msgin
-
-    array set msgin {}
-    port_read IN-1 msgin
-    if {$msgin(f1) != "ffff"} {
-		client_send "TESTFAIL"
-		return
-    }
-    port_write OUT-1 msgin
-	unset msgin
-
-    array set msgin {}
-    port_read IN-1 msgin
-    if {$msgin(f1) != "aaaa"} {
-		client_send "TESTFAIL"
-		return
-    }
-    port_write OUT-1 msgin
-	unset msgin
-
 	client_send "TESTEND"
-    array set msgin {}
-    port_read IN-1 msgin
 
     return
 }
